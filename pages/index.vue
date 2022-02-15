8de7435e38682edd8b2c936a75ee1301b8f1d4da
Lukas Enengel <lukas@enengel.at>
2022-02-15 21:07:07 +0100

    reinitial
    Install Strapi Frontend https://github.com/strapi/strapi-starter-nuxt-e-commerce
    
    1. Clone Git Repo https://github.com/strapi/strapi-starter-next-ecommerce/tree/master/starter
    2. npm install
    3. npm run-script develop

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="productsle">
+<Products :products="products" :error="error" :storeUrl="storeUrl" />
+</div>
+</template>
+
+<script>
+import Products from "~/components/Products.vue"
+
+export default {
+  data() {
+    return {
+      products: [],
+      storeUrl: process.env.storeUrl,
+      error: null
+    }
+  },
+  async mounted() {
+    try {
+      let result = await this.$strapi.$products.find()
+      
+      this.products = result.data;
+      console.log("products: ",this.products);
+      debugger;
+    
+    } catch (error) {
+      this.error = error
+    }
+  },
+  components: {
+    Products
+  }
+}
+</script>
+
